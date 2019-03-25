1672a27b5c519f442b2331e53bfe4b68b0b0c4a4
Mike <mike42@hotmail.de>
2019-03-25 20:35:15 +0100

    Interpreter changed

--- interpreter/src/main.cr
+++ interpreter/src/main.cr
@@ -7,7 +7,7 @@ code : String = ""
 if ARGV.size > 0
   file = ARGV[0]
   code = File.read(file)
-  puts "Brainf*ck with C*de '#{file}'"
+  puts "Brainf*ck Interpreter with C*de '#{file}'"
   interpreter = Interpreter.new(code)
   interpreter.run
 end
